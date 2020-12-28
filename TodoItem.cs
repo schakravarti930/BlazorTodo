fb5f6065264a6a547539259141788c02bee98864
Swapnil <schakravarti930@gmail.com>
2020-12-28 21:50:40 +0530

    Added event callbacks for select and delete and found a bug

--- TodoItem.cs
+++ TodoItem.cs
@@ -1,5 +1,12 @@
+using System.Threading;
 public class TodoItem
 {
+    static int nextId;
     public string Title{ get; set; }
     public bool IsDone { get; set; }
+    public int Id { get;private set; }
+    public TodoItem()
+    {
+        Id = Interlocked.Increment(ref nextId);
+    }
 }
